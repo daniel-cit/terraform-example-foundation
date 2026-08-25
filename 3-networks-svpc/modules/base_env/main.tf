c867bf2e8971e47eebe3359b63efe319dc7da4f4
Daniel Andrade <dandrade@ciandt.com>
2026-08-24 21:00:18 -0300

    add NCC on 3-networks-svpc

--- 3-networks-svpc/modules/base_env/main.tf
+++ 3-networks-svpc/modules/base_env/main.tf
@@ -16,27 +16,71 @@
 
 locals {
   bgp_asn_number = var.enable_partner_interconnect ? "16550" : "64514"
+  spoke_group    = "default"
+}
+
+data "google_compute_network" "vpc_dns_hub" {
+  count = var.environment_code != "p" ? 1 : 0
+
+  name    = "vpc-p-svpc"
+  project = local.dns_project_id
 }
 
 /******************************************
  Shared VPC
 *****************************************/
 module "shared_vpc" {
-  source = "../shared_vpc"
+  source = "git::https://github.com/daniel-cit/terraform-google-network.git//modules/foundation/network?ref=ncc-and-peering-changes"
 
-  project_id                   = local.shared_vpc_project_id
-  project_number               = local.shared_vpc_project_number
-  dns_project_id               = local.dns_project_id
-  environment_code             = var.environment_code
-  private_service_cidr         = var.private_service_cidr
-  private_service_connect_ip   = var.private_service_connect_ip
-  bgp_asn_subnet               = local.bgp_asn_number
-  default_region1              = var.default_region1
-  default_region2              = var.default_region2
-  domain                       = var.domain
-  target_name_server_addresses = var.target_name_server_addresses
+  project_id                 = local.shared_vpc_project_id
+  vpc_name                   = "${local.environment_code}-svpc"
+  shared_vpc_host            = true
+  resource_code              = local.environment_code
+  private_service_cidr       = var.private_service_cidr
+  private_service_connect_ip = var.private_service_connect_ip
+  firewall_enable_logging    = true
+  windows_activation_enabled = false
 
 
+  ncc_hub_config = {
+    create_hub   = true
+    name         = "ncc-hub-${local.env}"
+    description  = "NCC Hub for ${local.env}"
+    hub_labels   = { environment = local.env }
+    spoke_labels = { type = "hub_vpc" }
+
+    preset_topology              = "MESH"
+    spoke_group                  = "default"
+    auto_accept_projects_center  = null
+    auto_accept_projects_edge    = null
+    auto_accept_projects_default = [local.shared_vpc_project_id]
+  }
+
+  nat_config = {
+    enabled = false
+    bgp_asn = local.bgp_asn_number
+    regions = [
+      {
+        name          = local.default_region1
+        num_addresses = 2
+      },
+      {
+        name          = local.default_region2
+        num_addresses = 2
+      }
+    ]
+  }
+
+  dns_config = {
+    type                         = var.environment_code == "p" ? "hub" : "spoke"
+    enable_logging               = true
+    enable_inbound_forwarding    = var.environment_code == "p"
+    onprem_forwarding            = true
+    domain                       = var.domain
+    target_name_server_addresses = var.target_name_server_addresses
+    dns_hub_project_id           = var.environment_code == "p" ? null : local.dns_project_id
+    dns_hub_network_name         = var.environment_code == "p" ? null : data.google_compute_network.vpc_dns_hub[0].self_link
+  }
 
   subnets = [
     {
